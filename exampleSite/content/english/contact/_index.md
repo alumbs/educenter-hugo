fde7fabcc937bbac221225824ad0715998d31e4a
coolchibs <coolchibs@gmail.com>
2020-08-25 08:35:20 -0500

    added a test field to prevent spam

--- exampleSite/content/english/contact/_index.md
+++ exampleSite/content/english/contact/_index.md
@@ -3,5 +3,4 @@ title: Contact Us
 bg_image: images/backgrounds/page-title.jpg
 description: Contact us to discuss volunteer options, organizing a fundraiser for
   one of our causes or sharing a cause with us.
-
 ---
---
+++ static/js/checkFormSubmit.js
@@ -0,0 +1,10 @@
+function checkSubmit() {
+    // console.log('Form submit called');
+    // return false;
+  var test = document.getElementById("math-test").value;
+//   console.log('Test value is', test);
+  if (test !== 11) {
+    return false;
+  }
+  return false;
+}
