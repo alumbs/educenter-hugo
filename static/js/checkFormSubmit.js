d6e74deaefe9871a63df359198b38b03d3150787
coolchibs <coolchibs@gmail.com>
2020-08-25 08:37:30 -0500

    returned true

--- static/js/checkFormSubmit.js
+++ static/js/checkFormSubmit.js
@@ -1,10 +1,7 @@
 function checkSubmit() {
-    // console.log('Form submit called');
-    // return false;
   var test = document.getElementById("math-test").value;
-//   console.log('Test value is', test);
   if (test !== 11) {
     return false;
   }
-  return false;
+  return true;
 }
